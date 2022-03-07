71b9889d47131662e44a4bf6804697886e9b4713
Harshiya-Shaik <arshiyashaik7869@gmail.com>
2022-03-07 18:32:03 +0530

    hii

---
+++ PaymentBillingAdminLogin/src/main/java/com/domain2/PaymentBillingAdminLoginApplication.java
@@ -0,0 +1,16 @@
+package com.domain2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+//@ComponentScan("com.domain2")
+public class PaymentBillingAdminLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBillingAdminLoginApplication.class, args);
+	}
+
+}
